fa61604cabe94524843293bcabce5661932db4f0
Akshay Joshi <me@akshayjoshi.com>
2012-06-29 18:36:03 -0400

    Fix basic tests for users so that they pass.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name
+  validates_presence_of :first_name
+  validates_presence_of :last_name
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,11 @@
 require "minitest_helper"
 
 describe User do
+  it "must have a valid factory" do
+    user = FactoryGirl.build(:user)
+    user.valid?.must_equal true
+  end
+
   it "must have a first name" do
     user = FactoryGirl.build(:user)
     user.valid?.must_equal true
@@ -13,10 +18,5 @@ describe User do
     user.valid?.must_equal true
     user.last_name = nil
     user.valid?.must_equal false
-  end
-
-  it "must have a valid factory" do
-    user = FactoryGirl.build(:user)
-    user.valid?.must_equal true
   end
 end
