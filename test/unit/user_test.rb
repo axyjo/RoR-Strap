a41e20b9c22fbfbddad7acb054cb609155be282b
Akshay Joshi <me@akshayjoshi.com>
2012-06-29 18:31:06 -0400

    Add sample test for users.

---
+++ test/unit/user_test.rb
@@ -0,0 +1,22 @@
+require "minitest_helper"
+
+describe User do
+  it "must have a first name" do
+    user = FactoryGirl.build(:user)
+    user.valid?.must_equal true
+    user.first_name = nil
+    user.valid?.must_equal false
+  end
+
+  it "must have a last name" do
+    user = FactoryGirl.build(:user)
+    user.valid?.must_equal true
+    user.last_name = nil
+    user.valid?.must_equal false
+  end
+
+  it "must have a valid factory" do
+    user = FactoryGirl.build(:user)
+    user.valid?.must_equal true
+  end
+end
