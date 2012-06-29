c3d7531461d49bc1a31ceee629984497eb12630a
Akshay Joshi <me@akshayjoshi.com>
2012-06-29 17:41:07 -0400

    Add initial rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rorstrap::Application.config.secret_token = '6cd4a5787f4906ff93b6783c60555df2d6ad301b63b77c86b7ca15e083ffc5a5d0eb93f7e5ad7aba555947bd40efd9f1e73a23784c215db6815436acdf5c8725'
