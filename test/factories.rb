bc5a4e04f8a0ae240e39a445def87f14939d9003
Akshay Joshi <me@akshayjoshi.com>
2012-06-30 18:15:27 -0400

    Move factories to its own file.

--- test/factories.rb
+++ test/factories.rb
@@ -2,9 +2,15 @@ FactoryGirl.define do
   factory :authentication do
     provider 'facebook'
     uid '4'
-    after_build do |obj|
+    after(:build) do |obj|
       user = FactoryGirl.create(:user)
       obj.user_id  = user.id
     end
+  end
+
+  factory :user do
+    first_name 'John'
+    last_name 'Smith'
+    email 'john@example.com'
   end
 end
--- test/factories/user_factory.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    first_name 'John'
-    last_name 'Smith'
-    email 'john@example.com'
-  end
-end
