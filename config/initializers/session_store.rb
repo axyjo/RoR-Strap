33eafe69e5b3ccd340f311d46d4b667d20f04c60
Akshay Joshi <me@akshayjoshi.com>
2012-06-30 17:52:14 -0400

    Use ActiveRecord as the session store.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Rorstrap::Application.config.session_store :cookie_store, key: '_rorstrap_session'
+# Rorstrap::Application.config.session_store :cookie_store, key: '_rorstrap_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Rorstrap::Application.config.session_store :active_record_store
+Rorstrap::Application.config.session_store :active_record_store
