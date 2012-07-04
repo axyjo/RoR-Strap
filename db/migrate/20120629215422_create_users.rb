70a48a701c61155348dcd11e784ba30f7a3927a3
Akshay Joshi <me@akshayjoshi.com>
2012-07-03 23:37:03 -0400

    Add some metrics for users.

--- db/migrate/20120629215422_create_users.rb
+++ db/migrate/20120629215422_create_users.rb
@@ -3,6 +3,10 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
+      t.string :email
+      t.datetime :current_login_time
+      t.datetime :last_login_time
+      t.integer :login_count, :default => 0
 
       t.timestamps
     end
