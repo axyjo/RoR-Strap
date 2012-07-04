55b8f70c69c5a0bc5038df1295d1629d7aca1ab9
Akshay Joshi <me@akshayjoshi.com>
2012-07-03 23:50:40 -0400

    Optimizations for creating a new user.

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -14,7 +14,7 @@ class AuthenticationsController < ApplicationController
       user.update_from_omniauth(omniauth)
       user.last_login_time = user.current_login_time
       user.current_login_time = Time.now()
-      user.login_count++
+      user.login_count += 1
       user.save!
       if !omniauth.credentials.token.nil?
         auth.oauth_token = omniauth.credentials.token
@@ -31,14 +31,15 @@ class AuthenticationsController < ApplicationController
       u.current_login_time = Time.now()
       u.login_count = 1
       if u.save
-        auth = u.authentications.create!(:provider => omniauth['provider'], :uid => omniauth['uid'])
+        auth = u.authentications.new(:provider => omniauth['provider'], :uid => omniauth['uid'])
         if !omniauth.credentials.token.nil?
           auth.oauth_token = omniauth.credentials.token
           auth.oauth_expires = Time.at(omniauth.credentials.expires_at)
-          auth.save!
         end
-        UserMailer.signup_confirmation(u).deliver
-        session[:user_id] = u.id
+        if auth.save
+          UserMailer.signup_confirmation(u).deliver
+          session[:user_id] = u.id
+        end
       else
         flash[:notice] = "An account already exists. Please log in first."
       end
